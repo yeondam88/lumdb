41cc31d743a239602907ebd63601564303ba5eb9
Lloyd Park <yeondampark@lloyds-mbp.home>
2017-11-05 21:35:25 -0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Movie from "./Movie";
+
+const movies = [
+  {
+    id: 1,
+    title: "Iron Man"
+  },
+  {
+    id: 2,
+    title: "Spider Man"
+  },
+  {
+    id: 3,
+    title: "X man"
+  },
+  {
+    id: 4,
+    title: "Love story"
+  }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Movie.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Movie extends Component {
+  render() {
+    return (
+      <div>
+        <h3>{this.props.movie.title}</h3>
+        <p>{this.props.movie.desc}</p>
+      </div>
+    );
+  }
+}
